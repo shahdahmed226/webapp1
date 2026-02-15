d08e0a6e1cb76d46cce0970f3419640441d4b78e
shahdahmed226 <shoshoahmed202025@gmail.com>
2026-02-15 15:13:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,7 @@
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.get("/")
 def home():
-    return """
-    <html>
-      <body style="text-align:center;margin-top:120px;font-size:40px;">
-        Hello 👋<br/>My name is Shahd Hussein
-      </body>
-    </html>
-    """
+    return "Hello Shahd"
+
